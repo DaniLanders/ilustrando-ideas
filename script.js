faae977882f5deb8178c3938c91dda453c02c9df
DaniLanders <125817470+DaniLanders@users.noreply.github.com>
2023-03-04 20:37:28 -0600

    x7

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("HTMLCSS");
+        habilidades[1].classList.add("MSOFFIC");
+        habilidades[2].classList.add("powerapps");
+        habilidades[3].classList.add("git");
+        habilidades[4].classList.add("Visualstudiocode");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
         habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[9].classList.add("destreza");
     }
 }
 
